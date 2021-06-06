2bbe4e155bc62a644e48bc8e178b47bc792a91de
Nick Ligmann <nickligmann@Nicks-MBP.fritz.box>
2021-06-06 16:20:22 +0200

    Initial commit

---
+++ app/src/main/java/com/nickligmann/verze/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nickligmann.verze;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
